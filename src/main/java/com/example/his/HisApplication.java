ef8bd7ae01e9e2b3dfd3c876c9a56002318b18f7
wweichinese <wweichinese@126.com>
2024-07-14 19:13:33 +0800

    Initial commit of his

---
+++ src/main/java/com/example/his/HisApplication.java
@@ -0,0 +1,17 @@
+package com.example.his;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author 王伟
+ */
+@SpringBootApplication
+public class HisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HisApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/his/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.example.his.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author 王伟
+ * @date 2024/7/14 18:50
+ * @desc
+ */
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000") // 前端的地址
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
